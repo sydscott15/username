095f92503a24817443e7314b484178a1110c8b42
Sydney Scott <fahu@chapman.edu>
2020-11-17 23:42:38 -0800

    Adds username.sh

--- username.sh
+++ username.sh
@@ -1,13 +1,15 @@
 #! /bin/bash
 # username.sh
 # Sydney Scott 
-echo "Enter a five-digit ZIP code: "
-read ZIP
-file echo "$ZIP" | egrep -v "^[a-z][0-9]$"> /dev/null 2>&1
+echo "Enter a username: "
+read username
+file echo "$username" | egrep -v "^[a-z][0-9]$"> /dev/null 2>&1
 do
-	echo "You must enter a valid ZIP code - five digits only!"
-	echo "Enter a five-digit ZIP code: "
-	read ZIP
+	echo "You must enter a valid username - No more than 12 characters!"
+	echo "You must enter a valid username - only lowercase letters!"
+	echo "You must enter a valid username - only an underscore is an acceptable character!"
+	echo "Enter a username: "
+	read username
 done
 echo "Thank you"
 
